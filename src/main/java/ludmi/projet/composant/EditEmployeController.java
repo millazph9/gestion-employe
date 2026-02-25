2371f3e806f7cc8510beafff527dd05360037514
lzephir950 <ludmilla.zephir@gmail.com>
2026-02-25 18:04:30 +0100

    Création formulaire edit

--- src/main/java/ludmi/projet/composant/EditEmployeController.java
+++ src/main/java/ludmi/projet/composant/EditEmployeController.java
@@ -1,4 +1,43 @@
 package ludmi.projet.composant;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import ludmi.projet.app.Main;
+
+import java.io.IOException;
+
 public class EditEmployeController {
+
+
+    @FXML
+
+
+    private void onCloseEdit() {
+
+
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/fxml/Main.fxml"));
+        try {
+            Scene scene = new Scene(loader.load());
+            Main.stage.setScene(scene);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Annuler");
+    }
+
+    public void onSaveEdit() {
+
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/fxml/EditEmploye.fxml"));
+        try {
+            Scene scene = new Scene(loader.load());
+            Main.stage.setScene(scene);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Modifier");
+    }
 }
